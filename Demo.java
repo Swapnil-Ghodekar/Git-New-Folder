a7136747f01654074ed58273766acaf2980abd8b
Swapnil-Ghodekar <sghodekar0904@gmail.com>
2023-05-05 17:28:58 +0530

    ok

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+public static void main(String [] args)
+  {
+    System.out.println("Hello Swapnil");
+    System.out.println("Hello");
+   System.out.println("Jenkins");
+
+
+  }
+}
